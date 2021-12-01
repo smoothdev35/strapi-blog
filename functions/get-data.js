fa8e3a34715a0b497f38d94460ade742d446a40a
Hugo Priet <hugo.priet@gmail.com>
2021-12-01 16:51:21 -0600

    testin

--- functions/get-data.js
+++ functions/get-data.js
@@ -1,12 +1,12 @@
-import fetch from "node-fetch";
-
+//import fetch from "node-fetch";
+const bestsellers = require('./data/data.json');
 exports.handler = async () => {
-  const response = await fetch("http://localhost:1337/Articles")
+  /*const response = await fetch("http://localhost:1337/Articles")
     .then((res) => res.json())
-    .catch((err) => console.error(err));
+    .catch((err) => console.error(err));*/
 
   return {
     statusCode: 200,
-    body: JSON.stringify(response),
+    body: JSON.stringify(bestsellers),
   };
 };
